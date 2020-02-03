297382ae8318094f8d3889c43a14020b370071f7
developenguin <developenguin@users.noreply.github.com>
2020-02-03 22:27:07 +0100

    add synthetic drop events

--- src/components/Dropzone/index.jsx
+++ src/components/Dropzone/index.jsx
@@ -1,27 +1,64 @@
-import React, { createRef } from 'react';
+import React, { createRef, useState } from 'react';
 import propTypes from 'prop-types';
+import classNames from 'classnames';
 import styles from './styles.scss';
 
 const Dropzone = props => {
 
   let fileInputRef = createRef();
+  let [ isHovering, setIsHovering ] = useState(false);
 
   const onClickDropzone = () => {
     fileInputRef.current.click();
+  };
+
+  const onDragOver = evt => {
+    stopEvent(evt);
+    setIsHovering(true);
+  };
+
+  const onDragLeave = evt => {
+    stopEvent(evt);
+    setIsHovering(false);
+  };
+
+  const onDrop = evt => {
+
+    stopEvent(evt);
+
+    const { files } = evt.dataTransfer;
+
+    props.onFileChange(files);
+
+    setIsHovering(false);
+
   };
 
   const onFileChange = evt => {
 
     const { files } = evt.target;
 
-    props.onFileChange(Array.from(files));
+    props.onFileChange(files);
 
   };
 
+  const stopEvent = evt => {
+    evt.preventDefault();
+    evt.stopPropagation();
+  };
+
+  const classes = classNames({
+    [styles.container]: true,
+    [styles.containerHover]: isHovering
+  });
+
   return (
     <div
-      className={styles.container}
+      className={classes}
       onClick={onClickDropzone}
+      onDragOver={onDragOver}
+      onDragLeave={onDragLeave}
+      onDrop={onDrop}
     >
       <input
         type="file"
